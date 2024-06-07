74e14095d4187af5aa7ca4b570bcd2f05506cd49
unknown <yume31910@gmail.com>
2024-06-07 23:20:45 +0900

    [update]　random.dartのcolor_codeの変数をリスト型で管理するようにした

--- lib/presentation/pages/Random.dart
+++ lib/presentation/pages/Random.dart
@@ -8,11 +8,10 @@ class RandomScreen extends ConsumerWidget {
   });
 
   String color_code0 = '?????????';
-  String color_code1 = '?????????'; //色コード
+  String color_code1 = '?????????'; //色コード初期値
   String color_code2 = '?????????';
 
-  String toHex(Color color) {
-    //Colorを文字列の色コードに変換
+  String toHex(Color color) {    //Colorを文字列の色コードに変換
     final colorStr = color.value.toRadixString(16).toString();
     if (colorStr.length == 8) {
       final hexcolor = colorStr.substring(2);
@@ -32,10 +31,12 @@ class RandomScreen extends ConsumerWidget {
     // ここで ref.watch など
     double screenWidth = MediaQuery.of(context).size.width; //画面横幅習得
     double screenHeight = MediaQuery.of(context).size.height; //画面縦幅習得
-    List<Color> random_colors = ref.watch(randomcolorsNotifierProvider);
-    color_code0 = toHex(random_colors[0]);
-    color_code1 = toHex(random_colors[1]);
-    color_code2 = toHex(random_colors[2]);
+    List<Color> random_colors = ref.watch(randomcolorsNotifierProvider);//ランダム色取得
+    final color_code = List<String>.empty(growable: true);//カラーコードをリスト管理
+    int i;
+    for(i=0;i<3;i++){//カラーコードをセットする * 3回
+      color_code.add(toHex(random_colors[i]));
+    }
 
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 31, 31, 31),
@@ -55,7 +56,8 @@ class RandomScreen extends ConsumerWidget {
                   //width: 300,
                   width: screenWidth * 0.7,
                   height: screenHeight * 0.42,
-                  child: Text('$color_code0',
+                //  child: Text('$color_code0',
+                  child: Text('${color_code[0]}',
                       style: TextStyle(fontSize: 30.0, color: Colors.white)),
                 ),
               ),
@@ -71,7 +73,8 @@ class RandomScreen extends ConsumerWidget {
                 child: SizedBox(
                   width: screenWidth * 0.7,
                   height: screenHeight * 0.15,
-                  child: Text('$color_code1',
+               //   child: Text('$color_code1',
+                  child: Text('${color_code[1]}',
                       style: TextStyle(fontSize: 30.0, color: Colors.white)),
                 ),
               ),
@@ -87,7 +90,8 @@ class RandomScreen extends ConsumerWidget {
                 child: SizedBox(
                   width: screenWidth * 0.7,
                   height: screenHeight * 0.08,//高さを各々指定
-                  child: Text('$color_code2',//各々の色コードを指定
+                //  child: Text('$color_code2',//各々の色コードを指定
+                  child: Text('${color_code[2]}',//各々の色コードを指定
                       style: TextStyle(fontSize: 30.0, color: Colors.white)),
                 ),
               ),
